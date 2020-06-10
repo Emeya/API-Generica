44ad08b88e7c5eb3821fe7f39d5ceb5dcb9aca92
Manuel Soberanis <emeyappdeveloper_BK@hotmail.com>
2020-06-10 00:01:38 -0500

    Initial Commit

---
+++ API Generica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  API Generica
+//
+//  Created by Manuel Soberanis on 09/06/20.
+//  Copyright © 2020 Archer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
